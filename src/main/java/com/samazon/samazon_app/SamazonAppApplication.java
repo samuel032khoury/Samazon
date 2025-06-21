06adbf2cac12db912b475212d1da558100bee862
X. Samuel Ji <samuel.j032@icloud.com>
2025-06-22 00:01:18 +0800

    project init

---
+++ src/main/java/com/samazon/samazon_app/SamazonAppApplication.java
@@ -0,0 +1,13 @@
+package com.samazon.samazon_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamazonAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamazonAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samazon/samazon_app/SamazonAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samazon.samazon_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamazonAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
