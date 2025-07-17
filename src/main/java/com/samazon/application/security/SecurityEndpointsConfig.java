cfef483f4e09b7d068b08dba623f8b2b42a4d469
X. Samuel Ji <samuel.j032@icloud.com>
2025-07-17 15:14:12 +0800

    refactor: Clean up formatting in SecurityEndpointsConfig for better readability

--- src/main/java/com/samazon/application/security/SecurityEndpointsConfig.java
+++ src/main/java/com/samazon/application/security/SecurityEndpointsConfig.java
@@ -4,24 +4,23 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SecurityEndpointsConfig {
-    public static final String[] PUBLIC_ENDPOINTS = {
-            "/error",
-            "/api/auth/**",
-            "/api/public/**",
-            "/api/test/**",
-            "/h2-console/**",
-            "/v3/api-docs/**",
-            "/swagger-ui/**",
-            "/media/**",
-            "/api/admin/**" // TODO: Update to restrict access
-    };
+        public static final String[] PUBLIC_ENDPOINTS = {
+                        "/error",
+                        "/api/auth/**",
+                        "/api/public/**",
+                        "/api/test/**",
+                        "/h2-console/**",
+                        "/v3/api-docs/**",
+                        "/swagger-ui/**",
+                        "/media/**"
+        };
 
-    public static final String[] SWAGGER_ENDPOINTS = {
-            "/v2/api-docs",
-            "/swagger-resources/**",
-            "/swagger-ui.html",
-            "/webjars/**",
-            "/configuration/ui",
-            "/configuration/security"
-    };
+        public static final String[] SWAGGER_ENDPOINTS = {
+                        "/v2/api-docs",
+                        "/swagger-resources/**",
+                        "/swagger-ui.html",
+                        "/webjars/**",
+                        "/configuration/ui",
+                        "/configuration/security"
+        };
 }
