caf7ce996cb488f94c9e77314f1910152f270d1f
X. Samuel Ji <samuel.j032@icloud.com>
2025-07-17 15:37:55 +0800

    feat: Implement CartController with addToCart endpoint for adding items to the cart

---
+++ src/main/java/com/samazon/application/controllers/CartController.java
@@ -0,0 +1,27 @@
+package com.samazon.application.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.samazon.application.dto.CartDTO;
+import com.samazon.application.dto.requests.AddCartItemRequestDTO;
+import com.samazon.application.services.CartService;
+
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/api/cart")
+@AllArgsConstructor
+public class CartController {
+    private final CartService cartService;
+
+    @PostMapping("/add")
+    public ResponseEntity<CartDTO> addToCart(@Valid @RequestBody AddCartItemRequestDTO cartItemDTO) {
+        CartDTO cartDTO = cartService.addProductToCart(cartItemDTO.getProductId(), cartItemDTO.getQuantity());
+        return ResponseEntity.ok(cartDTO);
+    }
+}
