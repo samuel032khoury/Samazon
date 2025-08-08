0cee15235e07605513c0292c9616dada94f26c20
X. Samuel Ji <samuel.j032@icloud.com>
2025-08-08 23:18:31 +0800

    refactor: rename NewOrderRequestDTO

--- src/main/java/com/samazon/application/dto/requests/NewOrderRequestDTO.java
+++ src/main/java/com/samazon/application/dto/requests/NewOrderRequestDTO.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class AddOrderRequestDTO {
+public class NewOrderRequestDTO {
     private Long addressId;
     private String paymentMethod;
     private String paymentGatewayProvider;
