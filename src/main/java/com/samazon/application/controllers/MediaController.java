907a597593df520e55892a29ec3854a0208baa80
X. Samuel Ji <samuel.j032@icloud.com>
2025-08-12 15:44:17 +0800

    refactor: add TODO comment for cloud storage implementation in MediaController

--- src/main/java/com/samazon/application/controllers/MediaController.java
+++ src/main/java/com/samazon/application/controllers/MediaController.java
@@ -26,6 +26,7 @@ public class MediaController {
 
     @PostMapping("/media")
     public ResponseEntity<?> uploadMedia(@RequestParam("file") MultipartFile file) throws IOException {
+        // TODO: upload to cloud storage
         String fileName = fileService.uploadMedia(uploadDir, file);
         Map<String, String> response = Map.of(
                 "fileName", fileName,
