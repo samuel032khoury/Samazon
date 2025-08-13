da6d030e19974c5b9600720f9fb836024f79de85
X. Samuel Ji <samuel.j032@icloud.com>
2025-08-13 18:19:53 +0800

    refactor: replace ProductDTO with ProductResponse in CartItemDTO and remove unused ProductDTO class

--- src/main/java/com/samazon/application/dto/CartItemDTO.java
+++ src/main/java/com/samazon/application/dto/CartItemDTO.java
@@ -1,5 +1,7 @@
 package com.samazon.application.dto;
 
+import com.samazon.application.dto.products.ProductResponse;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,7 +12,7 @@ import lombok.NoArgsConstructor;
 public class CartItemDTO {
     private Long id;
     private Long cartId;
-    private ProductDTO product;
+    private ProductResponse product;
     private Integer quantity;
     private Double discount;
     private Double priceAtAddToCart;
