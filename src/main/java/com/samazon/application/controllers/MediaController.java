8b82cac87fdf5cf6cf2aaa6792053427fc8e6dc9
xiangxin <xiangxin.ji@smartx.com>
2025-08-15 17:45:21 +0800

    refactor: update uploadMedia method to return APIResponse for improved response structure

--- src/main/java/com/samazon/application/controllers/MediaController.java
+++ src/main/java/com/samazon/application/controllers/MediaController.java
@@ -1,7 +1,6 @@
 package com.samazon.application.controllers;
 
 import java.io.IOException;
-import java.util.Map;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.samazon.application.dto.common.APIResponse;
 import com.samazon.application.services.FileService;
 
 import lombok.RequiredArgsConstructor;
@@ -21,11 +21,8 @@ public class MediaController {
     private final FileService fileService;
 
     @PostMapping("/media")
-    public ResponseEntity<?> uploadMedia(@RequestParam("file") MultipartFile file) throws IOException {
+    public ResponseEntity<APIResponse> uploadMedia(@RequestParam("file") MultipartFile file) throws IOException {
         String fileName = fileService.uploadMedia(file);
-        Map<String, String> response = Map.of(
-                "fileName", fileName,
-                "message", "File uploaded successfully");
-        return ResponseEntity.ok(response);
+        return ResponseEntity.ok(new APIResponse("File " + fileName + " uploaded successfully", true));
     }
 }
