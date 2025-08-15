b1b8c02083ec867ad337897d63a050fba8f18857
X. Samuel Ji <samuel.j032@icloud.com>
2025-08-16 03:45:23 +0800

    refactor: remove redundant access modifiers for cleaner interface definition in CartService

--- src/main/java/com/samazon/application/services/CartService.java
+++ src/main/java/com/samazon/application/services/CartService.java
@@ -6,23 +6,23 @@ import com.samazon.application.dto.carts.CartResponse;
 import com.samazon.application.models.User;
 
 public interface CartService {
-    public CartResponse addProductToCart(Long productId, Integer quantity);
+    CartResponse addProductToCart(Long productId, Integer quantity);
 
-    public List<CartResponse> getAllCarts();
+    List<CartResponse> getAllCarts();
 
-    public CartResponse getCurrentUserCart();
+    CartResponse getCurrentUserCart();
 
-    public CartResponse updateCartItemQuantity(Long productId, Integer quantity, String action);
+    CartResponse updateCartItemQuantity(Long productId, Integer quantity, String action);
 
-    public void removeCartItem(Long productId);
+    void removeCartItem(Long productId);
 
-    public List<Long> getAllCartIdsWithProduct(Long productId);
+    List<Long> getAllCartIdsWithProduct(Long productId);
 
-    public List<Long> getAllCartIdsWithCategory(Long categoryId);
+    List<Long> getAllCartIdsWithCategory(Long categoryId);
 
-    public void recalculateCartTotal(Long cartId);
+    void recalculateCartTotal(Long cartId);
 
-    public void clearCart(Long id);
+    void clearCart(Long id);
 
-    public void createCartForUser(User newUser);
+    void createCartForUser(User newUser);
 }
