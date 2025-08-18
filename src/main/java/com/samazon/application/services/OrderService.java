b804aa37e1ad0d8703c3f9ac8db7bde25bf4ff2c
X. Samuel Ji <samuel.j032@icloud.com>
2025-08-18 23:00:45 +0800

    refactor: replace OrderDTO with OrderResponse in OrderService and OrderServiceImpl, updating order creation logic and enhancing response handling

--- src/main/java/com/samazon/application/services/OrderService.java
+++ src/main/java/com/samazon/application/services/OrderService.java
@@ -1,11 +1,11 @@
 package com.samazon.application.services;
 
 import com.samazon.application.dto.orders.OrderRequest;
-import com.samazon.application.dto.orders.OrderDTO;
+import com.samazon.application.dto.orders.OrderResponse;
 
 import jakarta.transaction.Transactional;
 
 public interface OrderService {
     @Transactional
-    OrderDTO placeOrder(OrderRequest request);
+    OrderResponse createOrder(OrderRequest request);
 }
